f63194d7ca3d9425a8c1deecdb42cc8517c4f83a
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-02-09 16:21:55 +0100

    Add middleware before save for hashing password

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const UserSchema = mongoose.Schema({
   username: {
@@ -26,6 +27,24 @@ const UserSchema = mongoose.Schema({
 
 // When the select propterty is set to false, whenever we query for a user the password is not returned.
 // Unless we ask for the password in the query
+
+// Middleware that runs before a user is saved
+UserSchema.pre("save", async function (next) {
+  // if the password isn't modified, it will not hash it again and call next()
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Salt (Salt length to generate or salt to use)
+  // which is the level of difficulty you want to use to hash the password
+  const salt = await bcrypt.genSalt(12);
+
+  // Hash the password with bcrypt
+  this.password = await bcrypt.hash(this.password, salt);
+
+  // save the user with the hashed password
+  next();
+});
 
 const User = mongoose.model("User", UserSchema);
 
