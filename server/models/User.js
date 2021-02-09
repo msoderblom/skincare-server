184b093ed8d35d477bdd692f0ad9a00fa6ff9b71
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-02-09 15:54:04 +0100

    Create User model

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+  },
+  passwords: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+// When the select propterty is set to false, whenever we query for a user the password is not returned.
+// Unless we ask for the password in the query
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
