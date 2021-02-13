1225b557980a3df8e19bccc453828f3dbd6a4d3a
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-02-13 15:32:49 +0100

    Add getAllThreads controller & route

--- server/controllers/forum.js
+++ server/controllers/forum.js
@@ -4,7 +4,7 @@ import ErrorResponse from "../utils/errorResponse.js";
 export const createThread = async (req, res, next) => {
   const { title, body } = req.body;
   if (!title || !body) {
-    return next(new ErrorResponse("", 400));
+    return next(new ErrorResponse("Please provide a title and body", 400));
   }
 
   try {
@@ -17,6 +17,44 @@ export const createThread = async (req, res, next) => {
     res.status(201).json({
       success: true,
       thread,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getAllThreads = async (req, res, next) => {
+  try {
+    let query = Thread.find(); // returns a promise
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 50;
+
+    // calculate which threads to get, how many documents to skip to get the docs we want
+
+    const skip = (page - 1) * pageSize;
+    const total = await Thread.countDocuments(); // Check how many threads there are in the database
+
+    // how many pages do we have (500/50 = 10)
+    const pages = Math.ceil(total / pageSize);
+
+    // skip() takes an argument of how many documents to skip
+    // limit() takes an argument of how many documents to get after the skip
+    query = query.sort({ createdAt: "desc" }).skip(skip).limit(pageSize);
+
+    // If the page is 11 and we have 10 total pages, throw error
+    if (page > pages) {
+      // have to add return so the nodemon server doesn't crash'
+      return next(new ErrorResponse("No page found", 404));
+    }
+
+    const result = await query;
+
+    res.status(200).json({
+      success: true,
+      count: result.length,
+      page,
+      pages,
+      data: result,
     });
   } catch (error) {
     next(error);
--- server/routes/forum.js
+++ server/routes/forum.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createThread } from "../controllers/forum.js";
+import { createThread, getAllThreads } from "../controllers/forum.js";
 import userAuth from "../middleware/auth.js";
 
 // Creating an express router
 const router = express.Router();
 
 // Defining routes
+router.get("/threads/", getAllThreads);
 router.post("/threads/", userAuth, createThread); // you need to be signed in to create a thread
 
 export default router;
