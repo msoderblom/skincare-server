fc52823fc74fee5ae2b29402743eeec306686ebe
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-02-24 16:58:33 +0100

    Add sidebar & header

--- server/admin/src/App.js
+++ server/admin/src/App.js
@@ -2,15 +2,33 @@ import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import BlogPage from "./pages/blog/BlogPage";
 import CreatePostPage from "./pages/blog/CreatePostPage";
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    // paddingLeft: drawerWidth + 30,
+    paddingTop: 70,
+  },
+}));
 const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      admin app
-      <Switch>
-        <Route exact path="/blog" component={BlogPage} />
-        <Route exact path="/blog/create-post" component={CreatePostPage} />
-      </Switch>
+    <div className={classes.root}>
+      <Header />
+      <Sidebar />
+      <main className={classes.content}>
+        <Switch>
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/blog/create-post" component={CreatePostPage} />
+        </Switch>
+      </main>
     </div>
   );
 };
---
+++ server/admin/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import * as S from "./styled";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" noWrap>
+          Clipped drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
