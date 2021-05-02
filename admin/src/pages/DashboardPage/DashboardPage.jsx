35bdb12d64c91182db31a8cdacc45b62569f3e81
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-05-02 16:49:26 +0200

    Add an empty DasboardPage

---
+++ admin/src/pages/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const DashboardPage = () => {
+  return (
+    <S.Container>
+      <p>DashboardPage</p>
+    </S.Container>
+  );
+};
+
+export default DashboardPage;
