5adab6433787d3ac56490524f0e15d6d671e07f7
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-05-02 16:48:40 +0200

    Add ProtectedRoute component

---
+++ admin/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
+import * as S from "./styled";
+
+const ProtectedRoute = ({ component: Component, ...restProps }) => {
+  const { admin } = useSelector((state) => state.admin);
+  return (
+    <Route
+      {...restProps}
+      render={(props) => {
+        if (admin) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
